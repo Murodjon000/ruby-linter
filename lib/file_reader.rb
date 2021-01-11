3e907a0ebc311c97faaef9734707b11242368bd9
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-11 13:41:01 +0500

    added file reader file to the lib

---
+++ lib/file_reader.rb
@@ -0,0 +1,16 @@
+require 'colorize'
+
+class FileReader
+    attr_reader :error_msg, :file_path, :file_lines, :file_lines_count
+    def initialize(file_path)
+        @error_msg=''
+        @file_path=file_path
+        begin
+            @file_lines=File.readline(@file_path)
+            @file_lines_count=@file_lines.size
+        rescue StandardError => exception
+            @file_lines=[]
+            @error_msg="Check file name or path one more time\n".colorize(:red) + exception.to_s.colorize(:light_red)
+        end
+    end
+end
