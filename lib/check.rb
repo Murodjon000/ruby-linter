2a6d3212517b47de96bbf0756cadafaed4f8e86b
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-11 15:15:27 +0500

    added whitespace error handler

--- lib/check.rb
+++ lib/check.rb
@@ -3,10 +3,21 @@ require 'strscan'
 require_relative './file_reader.rb'
 
 class CheckError
+    attr_reader :checker, :errors
+
     def initialize(file_path)
         @checker=FileReader.new(file_path)
-        errors=[]
+        @errors=[]
         @keywords=%w[begin case class def do if module unless]
     end
-    
+
+    def check_trailing_spaces
+        @checker.file_lines.each_with_index do |val,idx|
+            if val[-2] == ' ' && !val.strip.empty?
+                @errors << "line:#{idx + 1}:#{val.size-1}: Error: Trailing whitespace detected. "
+                + " '#{val.gsub(/\s*$/, '_')}' "
+            end
+        end
+    end
+
 end
