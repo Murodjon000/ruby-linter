7999e42793fa0f261ee4c8e4cc22769390bbbdbf
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-11 15:14:39 +0500

    added changes to main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts "Hello"
+require_relative '../lib/check.rb'
+
+check=CheckError.new(ARGV.first)
+check.check_trailing_spaces
+
+if check.errors.empty? && check.checker.error_msg.empty?
+    puts 'No offenses'.colorize(:green) + ' detected'
+else
+    check.errors.uniq.each do |er|
+        puts "#{check.checker.file_path.colorize(:blue)} : #{er.colorize(:red)}"
+    end
+end
+
+puts check.checker.error_msg if check.checker.file_lines.empty?
