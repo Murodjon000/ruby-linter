30718448e6ab7803c0b40a6a6e270e25ea115789
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-11 21:00:36 +0500

    fixed indentation in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../lib/check.rb'
 
-check=CheckError.new('../app.rb')
+check = CheckError.new('../app.rb')
 check.check_indentation
 check.check_trailing_spaces
 check.tag_error
@@ -9,11 +9,11 @@ check.end_error
 check.empty_line_error
 
 if check.errors.empty? && check.checker.error_msg.empty?
-    puts 'No offenses'.colorize(:green) + ' detected'
+  puts 'No offenses'.colorize(:green) + ' detected'
 else
-    check.errors.uniq.each do |er|
-        puts "#{check.checker.file_path.colorize(:green)} : #{er.colorize(:red)}"
-    end
+  check.errors.uniq.each do |er|
+    puts "#{check.checker.file_path.colorize(:green)} : #{er.colorize(:red)}"
+  end
 end
 
 puts check.checker.error_msg if check.checker.file_lines.empty?
