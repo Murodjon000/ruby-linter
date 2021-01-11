1f2913e6a203c53e74c977e7e2b553619c388c64
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-11 20:25:50 +0500

    changed indentations

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 require_relative '../lib/check.rb'
 
-check=CheckError.new(ARGV.first)
+check=CheckError.new('../app.rb')
+check.check_indentation
 check.check_trailing_spaces
+check.tag_error
+check.end_error
+check.empty_line_error
 
 if check.errors.empty? && check.checker.error_msg.empty?
     puts 'No offenses'.colorize(:green) + ' detected'
 else
     check.errors.uniq.each do |er|
-        puts "#{check.checker.file_path.colorize(:blue)} : #{er.colorize(:red)}"
+        puts "#{check.checker.file_path.colorize(:green)} : #{er.colorize(:red)}"
     end
 end
 
