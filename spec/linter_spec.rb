64337320a921fd1ad601470277b6f9be8dcabfd4
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-13 15:40:33 +0500

    fixed rspec test

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -7,7 +7,7 @@ describe CheckError do
   describe '#check_trailing_spaces' do
     it 'should return trailing space error' do
       checker.check_trailing_spaces
-      expect(checker.errors[0]).to eql('line:3:22: Error: Trailing whitespace detected.')
+      expect(checker.errors[0]).to eql('line:2:33: Error: Trailing whitespace detected.')
     end
   end
 
@@ -21,28 +21,28 @@ describe CheckError do
   describe '#tag_error' do
     it 'should return tag errors' do
       checker.tag_error
-      expect(checker.errors[0]).to eql("line:3 Lint/Syntax: Unexpected/Missing token '[' Square Bracket")
+      expect(checker.errors[0]).to eql("line:21 Lint/Syntax: Unexpected/Missing token '(' Parenthesis")
     end
   end
 
   describe '#check_end_empty_line' do
     it 'should return end errors' do
       checker.empty_line_error
-      expect(checker.errors[0]).to eql('line:11 Extra empty line detected at block body end')
+      expect(checker.errors[0]).to eql('line:23 Extra empty line detected at block body end')
     end
   end
 
   describe '#end_error' do
     it 'should return end errors' do
       checker.end_error
-      expect(checker.errors[0]).to eql("Lint/Syntax: Missing 'end'")
+      expect(checker.errors[0]).to eql("Lint/Syntax: Unexpected 'end'")
     end
   end
 
   describe '#empty_line_error' do
     it 'should return empty line errors' do
       checker.empty_line_error
-      expect(checker.errors[0]).to eql('line:11 Extra empty line detected at block body end')
+      expect(checker.errors[0]).to eql('line:23 Extra empty line detected at block body end')
     end
   end
 end
