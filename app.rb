dcb2e11239edf126eab8ab42a8b5c4eb66960389
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-13 15:00:08 +0500

    added more tests

--- app.rb
+++ app.rb
@@ -1,12 +1,24 @@
 class Board
   def initialize(player, board)
     @player = player] 
-     @board = board
+    @board = board
   end
 
   def board(lines)
-    lines.each do |x|
+    lines.each do  |x|
       puts x
     end
-  
+  end
+
+  def player_role(player)  
+    player.each_with_index do |val,ind| 
+      puts "Player name #{val}"
+      puts "Player index #{ind}"
+    end
+  end
+
+  def sum(a,b)
+    sum=a+b)  
+  end
+
 end
